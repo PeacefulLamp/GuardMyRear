b1b4016d4c5f75bad73a960fc4ab6db42090c429
hbirchtree <h0wy36@gmail.com>
2016-02-24 13:49:08 +0100

    - Add fragment for sensor data, only a button for now

--- app/src/main/java/com/pekka/guardmyrear/StreamActivity.java
+++ app/src/main/java/com/pekka/guardmyrear/StreamActivity.java
@@ -1,13 +1,19 @@
 package com.pekka.guardmyrear;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StreamActivity extends AppCompatActivity {
+public class StreamActivity extends AppCompatActivity implements SensorIndicatorFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stream);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        System.out.println(uri);
     }
 }
