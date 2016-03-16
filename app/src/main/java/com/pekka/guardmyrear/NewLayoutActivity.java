0bcb2fdec9218c762ec49e7749109054db6a2d6f
hbirchtree <h0wy36@gmail.com>
2016-03-16 14:59:06 +0100

    - Remove unused imports

--- app/src/main/java/com/pekka/guardmyrear/NewLayoutActivity.java
+++ app/src/main/java/com/pekka/guardmyrear/NewLayoutActivity.java
@@ -2,19 +2,14 @@ package com.pekka.guardmyrear;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 
 public class NewLayoutActivity extends AppCompatActivity {
 
