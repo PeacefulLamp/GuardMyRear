9b7a73736849a155d1d9e4ee3f4d9632d888b02f
hbirchtree <h0wy36@gmail.com>
2016-04-08 14:42:27 +0200

    - Add broadcast to sensor script  - Add dummy broadcaster

---
+++ RPI/dummy_sensor.py
@@ -0,0 +1,13 @@
+from socket import *
+import time
+import math
+
+cs = socket(AF_INET, SOCK_DGRAM)
+cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+for i in range(10000):
+	time.sleep(.01)
+	fake_data = 400*(math.sin(i*math.pi/1000)**2) + 100
+	print fake_data
+	cs.sendto("{\"key1\":%s, \"key2\":%s, \"key3\":%s}" %(fake_data, fake_data, fake_data), ('255.255.255.255', 5005))
--- RPI/sensor.py
+++ RPI/sensor.py
@@ -1,9 +1,10 @@
+from socket import *
+import math
 import RPi.GPIO as GPIO
 import time
 ​
 echoPin = 12 	#Pin 32 on Pi
 trigPin = 6		#Pin 31 on Pi
-​
 ​
 #Initialize pins
 GPIO.setmode(BCM)
@@ -34,4 +35,14 @@ def sensorRead():
 ​
 	distanceCenti = deltatime/(1000000.0 * 58)
 ​
-	print distanceCenti
+	return distanceCenti
+
+cs = socket(AF_INET, SOCK_DGRAM)
+cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+while True:
+	time.sleep(.01)
+	value_data = sensorRead()
+	print value_data
+	cs.sendto("{\"key1\":%s, \"key2\":%s, \"key3\":%s}" %(value_data, value_data, value_data), ('255.255.255.255', 5005))
